ba18d1343a24b95000daffa1579a803297a2b5c4
zbyang <zbyang@freewheel.com>
2025-06-12 14:45:31 +0800

    Update first version

--- src/index.js
+++ src/index.js
@@ -8,8 +8,106 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { ChatOpenAI } from "@langchain/openai";
+import {
+	HumanMessage
+} from "@langchain/core/messages";
+
+import { v4 as uuidv4 } from 'uuid';
+
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+		const url = new URL(request.url)
+		if (url.pathname !== "/v1/chat/completions") {
+			return new Response("Not found", { status: 404 })
+		}
+		const requestId = uuidv4()
+	  const requestObj = await request.json()
+		console.log(requestObj)
+		const { messages, stream = false, model = "doubao-1-5-pro-32k-250115", temperature = 0.7 } = requestObj
+		const llm = new ChatOpenAI({
+			modelName: model,
+			temperature,
+			streaming: stream,
+			apiKey: env.OPENAI_API_KEY,
+			configuration: {
+				baseURL: "https://ark.cn-beijing.volces.com/api/v3/",
+			},
+		})
+
+		const humanMessages = messages
+			.filter((m) => m.role === "user")
+			.map((m) => new HumanMessage(m.content))
+
+		let response = '';
+		if (!stream) {
+			const res = await llm.invoke(humanMessages)
+			return Response.json({
+				id: requestId,
+				object: "chat.completion",
+				created: Math.floor(Date.now() / 1000),
+				model,
+				choices: [{
+					index: 0,
+					message: { role: "assistant", content: res },
+					finish_reason: "stop",
+				}],
+			})
+		}
+
+		// ✅ SSE Streaming
+		const streamSSE = new ReadableStream({
+			async start(controller) {
+				const encoder = new TextEncoder()
+
+				const streamIterator = await llm.stream(humanMessages)
+
+				const created = Math.floor(Date.now() / 1000);
+
+				for await (const chunk of streamIterator) {
+					const token = chunk.content
+					response += token
+					const data = JSON.stringify({
+						id: requestId,
+						object: 'chat.completion.chunk',
+						model: model,
+						created,
+						choices: [
+							{
+								delta: { content: token, role: 'assistant' },
+								index: 0
+							}
+						]
+					})
+					controller.enqueue(encoder.encode(`data: ${data}\n\n`))
+					await new Promise(r => setTimeout(r, 0));
+				}
+				const doneChunk = JSON.stringify({
+					id: requestId,
+					object: 'chat.completion.chunk',
+					mode: model,
+					choices: [
+						{
+							delta: {content:'',role:'assistant'},
+							index: 0,
+							finish_reason: 'stop'
+						}
+					]
+				})
+				controller.enqueue(encoder.encode(`data: ${doneChunk}\n\n`))
+				controller.enqueue(encoder.encode(`data: [DONE]\n\n`))
+				controller.close()
+			},
+		})
+
+		return new Response(streamSSE, {
+			headers: {
+				"Content-Type": "text/event-stream",
+				"Cache-Control": "no-cache",
+				"Connection": "keep-alive",
+				"Transfer-Encoding": "chunked",
+				"X-Request-Id": requestId,
+			},
+		})
+	}
 };
